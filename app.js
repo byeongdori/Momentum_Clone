b2efc96b3b7a42886f6a152d88d73c1b3e4abc50
superbottlegod <superbottlegod@naver.com>
2021-07-19 16:10:30 +0900

    7/19

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ console.log(c);
 
 const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 daysOfWeek.push("sun");
+
+// object 생성 방법
+const player = {
+    name : "nico",
+    points: 10,
+    fat : true,
+};
+console.log(player);
+console.log(player.name);
+// 요소 추가도 가능
+player.lastName = "potato";
+
+// function 생성 방법
+function sayHello(nameOfPerson) {
+    console.log("Hello!" + nameOfPerson);
+}
+
+// object 안에 function , 선언 위치가 바뀜
+const player_insidefunction = {
+    name : "nico",
+    points: 10,
+    fat : true,
+    sayHello_insidefunction: function(nameOfPerson) {
+        console.log("Hello!" + nameOfPerson);
+    }
+}
