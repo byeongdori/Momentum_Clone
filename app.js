86535529d935870ca6078cfaa27f94a3df344bc1
superbottlegod <superbottlegod@naver.com>
2021-07-26 23:26:36 +0900

    7/26

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const a = 5;
 const b = 2;
-let c = 3
+let c = 3;
 
 console.log(a + b);
 console.log(a * b);
@@ -15,9 +15,9 @@ daysOfWeek.push("sun");
 
 // object 생성 방법
 const player = {
-    name : "nico",
-    points: 10,
-    fat : true,
+  name: "nico",
+  points: 10,
+  fat: true,
 };
 console.log(player);
 console.log(player.name);
@@ -26,24 +26,35 @@ player.lastName = "potato";
 
 // function 생성 방법
 function sayHello(nameOfPerson) {
-    console.log("Hello!" + nameOfPerson);
-};
+  console.log("Hello!" + nameOfPerson);
+}
 
 // object 안에 function , 선언 위치가 바뀜
 const player_insidefunction = {
-    name : "nico",
-    points: 10,
-    fat : true,
-    sayHello_insidefunction: function(nameOfPerson) {
-        console.log("Hello!" + nameOfPerson);
-    }
+  name: "nico",
+  points: 10,
+  fat: true,
+  sayHello_insidefunction: function (nameOfPerson) {
+    console.log("Hello!" + nameOfPerson);
+  },
 };
 
 // return 값이 있는 함수
 function calculateAge(age) {
-    return age + 2;
+  return age + 2;
 }
 
-// 
+// Conditional Part
 const age = parseInt(prompt("How old are you?"));
-console.log(age);
+
+if (isNaN(age) || age < 0) {
+  console.log("Please Write a real positive Number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
+}
