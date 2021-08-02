738305f8ac6a8fc0ae917d7539583201d9568ea3
superbottlegod <superbottlegod@naver.com>
2021-08-03 02:24:42 +0900

    8/2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// 기본 문법 //
+/*
 const a = 5;
 const b = 2;
 let c = 3;
@@ -12,8 +14,10 @@ console.log(c);
 
 const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 daysOfWeek.push("sun");
+*/
 
 // object 생성 방법 //
+/*
 const player = {
   name: "nico",
   points: 10,
@@ -28,8 +32,10 @@ player.lastName = "potato";
 function sayHello(nameOfPerson) {
   console.log("Hello!" + nameOfPerson);
 }
+*/
 
 // object 안에 function , 선언 위치가 바뀜 //
+/*
 const player_insidefunction = {
   name: "nico",
   points: 10,
@@ -38,13 +44,17 @@ const player_insidefunction = {
     console.log("Hello!" + nameOfPerson);
   },
 };
+*/
 
 // return 값이 있는 함수 //
+/*
 function calculateAge(age) {
   return age + 2;
 }
+*/
 
 // Conditional Part //
+/*
 const age = parseInt(prompt("How old are you?"));
 
 if (isNaN(age) || age < 0) {
@@ -58,23 +68,42 @@ if (isNaN(age) || age < 0) {
 } else if (age > 80) {
   console.log("You can do whatever you want.");
 }
+*/
 
 // JS - HTML 상호작용 //
 
 // id가 title인 요소 가져옴, 여러개인 경우 리스트로 받아옴
+/* 
 const title = document.getElementById("title");
 title.innerText = "Got You!";
 console.log(title.id);
-console.log(title.className);
+console.log(title.className); 
+*/
 
 // class가 hello인 요소 가져옴, 여러개인 경우 리스트로 받아옴
+/*
 const hellos = document.getElementsByClassName("hello");
+*/
 
 // tag가 h1인 요소 가져옴, 여러개인 경우 리스트로 받아옴
+/*
 const h1tag = document.getElementsByTagName("h1");
+*/
 
 // query이용해 요소 찾기
 // 단, 같은 게 있으면 첫번째만 가져옴, 모두 가져오고 싶으면,
 // querySelectorAll 사용
+/*
 const title_searchbyclass_and_tag = document.querySelector(".hello h1");
 const title_searchbyId = document.querySelector("#hello");
+*/
+
+// Event 관리 //
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+// 클릭하면 handleTitleClick 함수 실행!
+title.addEventListener("click", handleTitleClick);
