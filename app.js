a5f1ed6b5b8724ebea4fda8f808e30696012a703
superbottlegod <superbottlegod@naver.com>
2021-08-11 14:11:44 +0900

    8/11

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const loginInput = loginForm.querySelector("#login-form input");
-const loginButton = loginForm.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function onLoginButtonClick() {
-    console.log("click!!!");
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  // preventDefault 함수 -> submit 해도 새로고침 안되게 막아줌!
+  event.preventDefault();
+  console.log(username);
 }
-loginButton.addEventListener("click", onLoginButtonClick);
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    alert("clicked");
+}
+ 
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
