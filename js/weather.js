e4859d87280972f4dc0b3ac4b66d36ced29a89b6
superbottlegod <superbottlegod@naver.com>
2022-01-10 20:13:02 +0900

    1/10 - apply css

--- js/weather.js
+++ js/weather.js
@@ -4,20 +4,22 @@ const API_KEY = "1afb65b027e5061da2c2d6443125a2b8";
 
 // position -> 사용자의 위치정보가 담겨있는 매개변수
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
 
-    const APIurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(APIurl).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°`;
+  const APIurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(APIurl)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.getElementById("weather-first");
+      const city = document.getElementById("weather-second");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}° `;
     });
 }
 
 function onGeoError() {
-    alert("Can't find you. No weather for you");
+  alert("Can't find you. No weather for you");
 }
 
 // 사용자의 위치를 받아오는 함수
